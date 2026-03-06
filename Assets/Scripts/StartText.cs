4985fdfdf739f524a45a495c082ee411b739b903
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-03-06 03:40:52 -0800

    Updated race start animation

--- Assets/Scripts/StartText.cs
+++ Assets/Scripts/StartText.cs
@@ -1,11 +1,13 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartText : MonoBehaviour, StartListener {
     CanvasGroup canvasGroup;
 
     public TextMeshProUGUI cutoutText;
     public TextMeshProUGUI overlayText;
+    public Image backgroundImage;
 
     void Start() {
         canvasGroup = GetComponent<CanvasGroup>();
@@ -17,19 +19,32 @@ public class StartText : MonoBehaviour, StartListener {
     public void NotifyCountdownUpdated(int countdown) {
         if (countdown == 2) {
             canvasGroup.alpha = 1;
-        } else if (countdown == 1) {
-            overlayText.alpha = 1;
-            cutoutText.transform.localScale = Vector3.one;
             
-            LeanTween.value(gameObject, 1, 0, 1F).setOnUpdate((value) => {
-                overlayText.alpha = value;
-            });
+            overlayText.alpha = 0;
+            cutoutText.alpha = 1;
+            Color color = backgroundImage.color;
+            backgroundImage.color = new Color(color.r, color.g, color.b, 1);
+            
+            cutoutText.transform.localScale = Vector3.one;
+        } else if (countdown == 1) {
+            cutoutText.alpha = 0;
+            overlayText.alpha = 1;
+            Color color = backgroundImage.color;
+            backgroundImage.color = new Color(color.r, color.g, color.b, 0);
+            
+            // LeanTween.value(gameObject, 1, 0, 1F).setOnUpdate((value) => {
+            //     overlayText.alpha = value;
+            // });
         } else if (countdown <= 0) {
-            canvasGroup.alpha = 0;
+            // canvasGroup.alpha = 0;
 
-            LeanTween.value(gameObject, 1, 5, 1F).setOnUpdate((value) => {
+            LeanTween.value(gameObject, 1, 10, 10F).setOnUpdate((value) => {
                 cutoutText.transform.localScale = Vector3.one * value;
-            });
+            }).setEase(LeanTweenType.easeOutExpo);
+            
+            // LeanTween.value(gameObject, 1, 0, 15F).setOnUpdate((value) => {
+            //      canvasGroup.alpha = value;
+            // }).setEase(LeanTweenType.easeOutExpo);
         }
     }
 
