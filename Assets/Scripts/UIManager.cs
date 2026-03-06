35aaf3a9a94aed5b5344d6db92d941aaa4a12b6e
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-03-06 02:15:43 -0800

    Split effect is on timer

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,14 +12,14 @@ public class UIManager : MonoBehaviour {
     }
 
     public void DisplaySplit() {
-        splitText.text = Timer.GetFormattedTime(Timer.Instance.GetTimer());
+        // splitText.text = Timer.GetFormattedTime(Timer.Instance.GetTimer());
         
         LeanTween.cancel(splitText.gameObject);
         
         Color color = splitText.color;
         
-        splitText.transform.localScale = Vector2.one * 1.25F + new Vector2(0, 0.75F);
-        splitText.color = new Color(color.r, color.g, color.b, 0);
+        // splitText.transform.localScale = Vector2.one * 1.25F + new Vector2(0, 0.75F);
+        // splitText.color = new Color(color.r, color.g, color.b, 0);
 
         LeanTween.value(splitText.gameObject, splitText.transform.localScale.x, 1, 0.5F).
             setOnUpdate((xScale) => { splitText.transform.localScale = new Vector2(xScale, splitText.transform.localScale.y); }).
@@ -27,17 +27,17 @@ public class UIManager : MonoBehaviour {
         LeanTween.value(splitText.gameObject, splitText.transform.localScale.y, 1, 0.5F).
             setOnUpdate((yScale) => { splitText.transform.localScale = new Vector2(splitText.transform.localScale.x, yScale); }).
             setEase(LeanTweenType.easeOutBounce);
-        LeanTween.value(splitText.gameObject, 0, 1, 0.15F).setOnUpdate((alpha) => {
-            splitText.color = new Color(color.r, color.g, color.b, alpha);
-        });
+        // LeanTween.value(splitText.gameObject, 0, 1, 0.15F).setOnUpdate((alpha) => {
+        //     splitText.color = new Color(color.r, color.g, color.b, alpha);
+        // });
         
-        LeanTween.delayedCall(splitText.gameObject, 4F, () => {
-            LeanTween.value(splitText.gameObject, splitText.transform.localScale.x, 0.75F, 3F).
-                setOnUpdate((scale) => { splitText.transform.localScale = new Vector2(scale, scale); }).
-                setEase(LeanTweenType.easeOutExpo);
-            LeanTween.value(splitText.gameObject, 1, 0, 0.5F).setOnUpdate((alpha) => {
-                splitText.color = new Color(color.r, color.g, color.b, alpha);
-            }).setEase(LeanTweenType.easeOutExpo);
-        });
+        // LeanTween.delayedCall(splitText.gameObject, 4F, () => {
+        //     LeanTween.value(splitText.gameObject, splitText.transform.localScale.x, 0.75F, 3F).
+        //         setOnUpdate((scale) => { splitText.transform.localScale = new Vector2(scale, scale); }).
+        //         setEase(LeanTweenType.easeOutExpo);
+        //     LeanTween.value(splitText.gameObject, 1, 0, 0.5F).setOnUpdate((alpha) => {
+        //         splitText.color = new Color(color.r, color.g, color.b, alpha);
+        //     }).setEase(LeanTweenType.easeOutExpo);
+        // });
     }
 }
