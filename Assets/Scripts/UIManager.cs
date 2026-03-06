7960677cf0830c4965f3fbb9bff99360edd3d2a4
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-03-06 02:20:40 -0800

    Adjusted split text animation

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -18,13 +18,13 @@ public class UIManager : MonoBehaviour {
         
         Color color = splitText.color;
         
-        // splitText.transform.localScale = Vector2.one * 1.25F + new Vector2(0, 0.75F);
+        splitText.transform.localScale = Vector2.one * 1.25F + new Vector2(0, 0.75F);
         // splitText.color = new Color(color.r, color.g, color.b, 0);
 
-        LeanTween.value(splitText.gameObject, splitText.transform.localScale.x, 1, 0.5F).
+        LeanTween.value(splitText.gameObject, splitText.transform.localScale.x, 1, 0.7F).
             setOnUpdate((xScale) => { splitText.transform.localScale = new Vector2(xScale, splitText.transform.localScale.y); }).
             setEase(LeanTweenType.easeOutBounce);
-        LeanTween.value(splitText.gameObject, splitText.transform.localScale.y, 1, 0.5F).
+        LeanTween.value(splitText.gameObject, splitText.transform.localScale.y, 1, 0.7F).
             setOnUpdate((yScale) => { splitText.transform.localScale = new Vector2(splitText.transform.localScale.x, yScale); }).
             setEase(LeanTweenType.easeOutBounce);
         // LeanTween.value(splitText.gameObject, 0, 1, 0.15F).setOnUpdate((alpha) => {
