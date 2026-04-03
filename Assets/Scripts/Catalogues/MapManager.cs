6afa2f43226c7b6577ca9fef9735b6ed2f37439e
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-04-02 18:21:32 -0700

    Removed Flapjack Raceway

--- Assets/Scripts/Catalogues/MapManager.cs
+++ Assets/Scripts/Catalogues/MapManager.cs
@@ -24,40 +24,17 @@ public class MapManager {
     public MapData GetSelectedMap() {
         return maps[selectedMapIndex];
     }
-    
+
     readonly List<MapData> maps = new() {
         new MapData(0, "Dusty Desert", new Color(0.811F, 0.38F, 0.09F, 0.75F)),
         new MapData(1, "Sneaky Snow", new Color(0, 0.43F, 1, 0.75F)),
         new MapData(2, "Pink Plains", new Color(1, 0.16F, 0.33F, 0.75F)),
         new MapData(3, "Akina Downhill", new Color(1, 0.12F, 0, 0.75F)),
-        new MapData(4, "Flapjack Raceway", new Color(1, 0.12F, 0, 0.75F))
+        // new MapData(4, "Flapjack Raceway", new Color(1, 0.12F, 0, 0.75F))
     };
-
-    public MapData GetMapAtIndex(int index) {
-        return maps[index];
-    }
     
     public int MapCount() {
         return maps.Count;
-    }
-    
-    public int GetStars(int map, float time) {
-        var result = 0;
-        // if (time <= maps[map].times[2]) {
-        //     result = 3;
-        // }
-        // else if (time <= maps[map].times[1]) {
-        //     result = 2;
-        // }
-        // else if (time <= maps[map].times[0]) {
-        //     result = 1;
-        // }
-        //
-        // if (time <= 0f) {
-        //     result = 0;
-        // }
-        
-        return result;
     }
 
     public class MapData {
